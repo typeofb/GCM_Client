0ef59292ee6e75ec8bc02f24281ffead0c5641a5
WORK-PC\typeo <typeofb@gmail.com>
2019-10-03 11:11:37 +0900

    주석 수정

--- app/src/main/java/com/example/gcm_client/lib/MsgHandler.java
+++ app/src/main/java/com/example/gcm_client/lib/MsgHandler.java
@@ -7,7 +7,7 @@ import androidx.fragment.app.FragmentActivity;
 public abstract class MsgHandler extends FragmentActivity {
     protected Handler MsgHandler = new Handler() {
         @Override
-        public void handleMessage(Message msg) {  // 메시지 수신
+        public void handleMessage(Message msg) {  // ③ 메시지 수신
             if (msg.what == 0) {
 //                sendActivityUpdate(msg.arg1, msg.arg2);
             }
@@ -15,8 +15,8 @@ public abstract class MsgHandler extends FragmentActivity {
     };
 
     public void sendActivityUpdate(int arg1, int arg2) {
-        Message m = Message.obtain(this.MsgHandler, 0, arg1, arg2);  // 메시지 요청
-        this.MsgHandler.sendMessage(m);  // 메시지 전송
+        Message m = Message.obtain(this.MsgHandler, 0, arg1, arg2);  // ① 메시지 대상 설정 to this.MsgHandler
+        this.MsgHandler.sendMessage(m);  // ② 메시지 송신
 
         try {
             Thread.sleep(100);
